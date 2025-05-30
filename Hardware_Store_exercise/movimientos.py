f188db91321c7c4bc73d0290bcd75d310a585412
edwinlga <edwinlga@gmail.com>
2025-05-30 06:08:00 -0400

    Hardware_Store_exercise/productos.py

--- Hardware_Store_exercise/movimientos.py
+++ Hardware_Store_exercise/movimientos.py
@@ -1,26 +1,30 @@
 from productos import Producto
+
+
 class Movimiento:
 
     def __init__(self, fecha, tipo, cantidad):
-        self.fecha=fecha
-        self.tipo=tipo
-        self.cantidad=cantidad
+        self.fecha = fecha
+        self.tipo = tipo
+        self.cantidad = cantidad
 
     def mostrar_todos():
         pass
 
-    def agregar(self, sku):
+    @classmethod
+    def agregar(cls, sku, fecha, tipo, cantidad):
         item = Producto.mostrar_por_sku(sku)
         movimientos = item[0]['movimientos']
+        print(type(movimientos))
         movimientos.append({
-            'fecha:' self.fecha,
-            'tipo:' self.tipo,
-            'cantidad:' self.cantidad
+            'palabra': 'hola',
+            'otra': 'mas palabras'
             })
+        print(movimientos)
         
-        """item['movimientos'].append({
-            
-        })"""
+
+       
+
 
 if __name__ == '__main__':
     mov = Movimiento('25/01/2025', 'E', 10)
--- Hardware_Store_exercise/productos.py
+++ Hardware_Store_exercise/productos.py
@@ -1,60 +1,79 @@
+from tabulate import tabulate
 from data.db_productos import productos
+
 
 class Producto:
 
-  def __init__(self, sku, categoria, nombre, descripcion, precio_unitario, cantidad_bodega):
+    def __init__(self, sku, categoria, nombre, descripcion, precio_unitario, cantidad_bodega):
 
-    # ::::::::::: Aplicando la programacion defensiva :::::::::::
+        # ::::::::::: Aplicando la programacion defensiva :::::::::::
 
-    # Validando que no haya parametros vacios o None
-    if not all([sku, categoria,nombre,descripcion,precio_unitario,cantidad_bodega]):
-      return 0
+        # Validando que no haya parametros vacios o None
+        if not all([sku, categoria, nombre, descripcion, precio_unitario, cantidad_bodega]):
+            return 0
 
-    # Validando que precio_unitario sea numerico
-    if not isinstance(precio_unitario, (int, float)) or precio_unitario <= 0:
-      return 0
+        # Validando que precio_unitario sea numerico
+        if not isinstance(precio_unitario, (int, float)) or precio_unitario <= 0:
+            return 0
 
-    # Validando que cantida_bodega sea numerico e igual o mayor que cero
-    if not isinstance(precio_unitario, (int, float)) or precio_unitario <= 0:
-      return 0
+        # Validando que cantida_bodega sea numerico e igual o mayor que cero
+        if not isinstance(precio_unitario, (int, float)) or precio_unitario <= 0:
+            return 0
 
-    self.sku=sku
-    self.categoria=categoria
-    self.nombre=nombre
-    self.descripcion=descripcion
-    self.precio_unitario=precio_unitario
-    self.cantidad_bodega=cantidad_bodega
-    # Movimientos
-    self.movimientos=[]
+        self.sku = sku
+        self.categoria = categoria
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio_unitario = precio_unitario
+        self.cantidad_bodega = cantidad_bodega
+        # Movimientos
+        self.movimientos = []
 
-  def agregar(self):
-    
-    productos.append({
-        'sku': self.sku,
-        'categoria': self.categoria,
-        'nombre': self.nombre,
-        'descripcion': self.descripcion,
-        'precio_unitario': self.precio_unitario,
-        'cantidad_bodega': self.cantidad_bodega
+    def agregar(self):
 
-    })
+        productos.append({
+            'sku': self.sku,
+            'categoria': self.categoria,
+            'nombre': self.nombre,
+            'descripcion': self.descripcion,
+            'precio_unitario': self.precio_unitario,
+            'cantidad_bodega': self.cantidad_bodega
+
+        })
+
+    @classmethod
+    def existe_sku(cls, sku):
+        "Comprueba si el SKU ya ha sido asignado a un producto"
+        lista_sku = [p['sku'] for p in productos]
+        return sku in lista_sku
+
+    @classmethod
+    def mostrar_por_sku(cls, sku):
+        "Mostrar productos por SKU"
+        if Producto.existe_sku(sku):
+            detalles = [p.values() for p in productos if p['sku'] == sku]
+            print(detalles)
+            #print(tabulate([['SKU', 'CATEGORIA', 'NOMBRE', 'DESCRIPCION', 'PRECIO', 'CANTIDAD', 'MOVIMIENTOS'], detalles], headers='firstrow'))
+        else:
+            return None
+
+    @classmethod
+    def mostrar_listado(cls):
+      listado=[
+        ['SKU', 'CATEGORIA', 'NOMBRE', 'DESCRIPCION', 'PRECIO', 'CANTIDAD', 'MOVIMIENTOS']
+      ]
+      for p in productos:
+        listado.append(p.values())
+      
+      print(tabulate(listado, headers='firstrow'))
 
 
-  @classmethod
-  def existe_sku(cls, sku):
-    "Comprueba si el SKU ya ha sido asignado a un producto"
-    lista_sku =[p['sku'] for p in productos]
-    return sku in lista_sku
 
-
-  @classmethod
-  def mostrar_por_sku(cls, sku):
-    "Mostrar productos por SKU"
-    if Producto.existe_sku(sku):
-      return [p for p in productos if p['sku']==sku]
-    else:
-      return None
-
-  
 if __name__ == "__main__":
-  print(Producto.mostrar_por_sku(22345678))
+
+    producto = Producto(10345678, 'Herramientas', 'Alicate',
+                        'Alicate de electricista', 150, 20)
+    producto.agregar()
+    #Producto.mostrar_listado()
+    Producto.mostrar_por_sku(10345678)
+    
